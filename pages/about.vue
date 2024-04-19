6e17d003815e5b198ffdd63cb89d4f23dc43c61a
Tarik Kavaz <tarikkavaz@gmail.com>
2024-04-19 09:37:55 +0300

    Update i18n configuration and components for localization support

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-4 mb-4 bg-purple-400">
-    <h1>About Page</h1>
+    <h1>{{ $t('about') }}</h1>
     <h2>{{ $t('welcome') }}</h2>
   </div>
   <AppAlert>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-4 mb-4 bg-purple-400">
-    <h1>Home Page</h1>
+    <h1>{{ $t('home') }}</h1>
     <h2>{{ $t('welcome') }}</h2>
   </div>
   <BilgiNews />
