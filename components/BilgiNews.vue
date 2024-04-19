0c25b435a18808a10a457e16dee9d3c553ca6ea8
Tarik Kavaz <tarikkavaz@gmail.com>
2024-04-19 15:02:58 +0300

    splitting en / tr news

--- components/BilgiNews.vue
+++ components/BilgiNews.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="">
+  <div>
     <div v-if="pending" class="p-4 mb-2 bg-orange-300 last:mb-0">{{ $t('loading') }}</div>
-    <div v-else-if="newsData && newsData.results.length">
-      <NuxtLink :to="item.url" v-for="item in newsData.results" :key="item.id" class="flex gap-4 p-4 mb-2 bg-orange-300 last:mb-0" target="_blank">
+    <div v-else-if="filteredNews.length">
+      <NuxtLink :to="item.url" v-for="item in filteredNews" :key="item.id" class="flex gap-4 p-4 mb-2 bg-orange-300 last:mb-0" target="_blank">
         <img :src="item.image" :alt="item.short_16words" class="w-auto max-h-20">
         <div>
           <p class="text-2xl">{{ item.title }}</p>
@@ -15,37 +15,42 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
-import { useLazyAsyncData } from '#imports';
+  import { ref, watch, computed } from 'vue';
+  import { useI18n } from 'vue-i18n'; 
+  import { useFetch } from 'nuxt/app';
 
-// Define a type for the item structure
-interface NewsItem {
-  id: string;
-  title: string;
-  short_16words: string;
-  url: string;
-  image: string;
-}
-
-interface NewsResult {
-  results: NewsItem[];
-}
-
-// Async data fetching with proper typing
-const { pending, data: newsDataRef } = await useLazyAsyncData<NewsResult | null>('items', () => $fetch('https://www.bilgi.edu.tr/api/news/?site=test.bilgi.edu.tr'));
-
-// Unwrap the ref for easier access and initialize it safely
-const newsData = ref<NewsResult>({ results: [] });
-if (newsDataRef.value) {
-  newsData.value = newsDataRef.value;
-}
-
-// Watch for changes in data
-watch(newsDataRef, (newData) => {
-  if (newData) {
-    newsData.value = newData;
-  } else {
-    newsData.value = { results: [] };
+  interface NewsItem {
+    id: string;
+    title: string;
+    short_16words: string;
+    url: string;
+    image: string;
   }
-});
+
+  interface NewsResult {
+    results: NewsItem[];
+  }
+
+  const { pending, data: newsDataRef } = await useFetch<NewsResult>('https://www.bilgi.edu.tr/api/news/?site=test.bilgi.edu.tr');
+
+  const newsData = ref<NewsResult>({ results: [] });
+
+  if (newsDataRef.value) {
+    newsData.value = newsDataRef.value;
+  }
+
+  watch(newsDataRef, (newData, oldData) => {
+    if (newData) {
+      newsData.value = newData;
+    } else {
+      newsData.value = { results: [] };
+    }
+  });
+
+  const { locale } = useI18n();
+
+  const filteredNews = computed(() => {
+    const localePathSegment = locale.value === 'en' ? '/en/' : '/tr/';
+    return newsData.value.results.filter(item => item.url.includes(localePathSegment));
+  });
 </script>
