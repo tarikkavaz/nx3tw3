b418fde775e122aa46799fe427f1d3398dcadb48
Tarik Kavaz <tarikkavaz@gmail.com>
2024-04-22 11:08:12 +0300

    Update error.vue button text to use 'index' instead of 'home'

--- error.vue
+++ error.vue
@@ -2,7 +2,7 @@
   <NuxtLayout>
     <div class="p-4 mb-4 bg-purple-400" id="content">
       <h1>You've Arrived Here on Error</h1>
-      <button class="button" @click="goBack">{{ $t('home') }}</button>
+      <button class="button" @click="goBack">{{ $t('index') }}</button>
     </div>
   </NuxtLayout>
 </template>
